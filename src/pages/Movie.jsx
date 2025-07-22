df33151ced1b7b806da3722a6b94ccc314bac44f
ka-few <njorovista@gmail.com>
2025-07-22 08:10:10 +0300

    Add Basic routes

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,13 +1,34 @@
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4001/movies/${id}`)
+      .then((r) => r.json())
+      .then(setMovie)
+      .catch((err) => console.error("Failed to fetch movie", err));
+  }, [id]);
+
+  if (!movie) return <p>Loading movie...</p>;
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <div>
+        <h1>{movie.title}</h1>
+        <p>{movie.time}</p>
+        <div>
+          {movie.genres.map((genre, index) => (
+            <span key={index}>{genre}</span>
+          ))}
+        </div>
+      </div>
       </main>
     </>
   );
